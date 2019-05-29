49a056948400c3cf2e4c40f60e8f2c95b190ffc1
SeN <sebastimus@o2.pl>
2019-05-29 17:00:42 +0200

    adding new icons

--- src/components/Post.js
+++ src/components/Post.js
@@ -28,15 +28,42 @@ export default function Post(props) {
     // // console.log(arraCop.map(ee => ee.body));
     return (
         <div className="container-posts-main">
-            {arraCop.map((postsUsers, i) => 
-                {
-                    return (
-                        <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m" key={i}>
-                            <h3 class="uk-card-title">{postsUsers.title}</h3>
-                            <p>Lorem ipsum <a href="#">dolor</a>{postsUsers.body}</p>
+            <div className="header-posts">
+                <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid="false">
+                    <div>
+                        <div class="uk-card uk-card-body main-cards-posts-left">
+                            <h3 class="uk-card-title">
+                                <Link className="arrow-back" to="/">
+                                    <span uk-icon="icon: reply; ratio: 2"></span>Back
+                                </Link>
+                            </h3>
                         </div>
-                    );
-                })
+                    </div>
+                    <div>
+                        <div class="uk-card uk-card-body main-cards-posts-center">
+                            <h3 class="uk-card-title">Author</h3>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-card uk-card-body main-cards-posts-right">
+                            <h3 class="uk-card-title">
+                                <Link className="arrow-back" to="/#">
+                                    <span uk-icon="icon:  plus-circle; ratio: 2"></span>
+                                </Link>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {arraCop.map((postsUsers, i) => {
+                return (
+                    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m" key={i}>
+                        <h3 class="uk-card-title">{postsUsers.title}</h3>
+                        <p>{postsUsers.body}</p>
+                        <p><a href="#">View comments</a> </p>
+                    </div>
+                );
+            })
             }
         </div>
     )
