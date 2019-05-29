1fa3854b0e93816e756852d2d1917673b82b15ba
SeN <sebastimus@o2.pl>
2019-05-29 16:07:22 +0200

    adding to method comparing user and id

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,22 +6,36 @@ export default function Post (props) {
 
     const [postsArray, setPostsArray] = useState([]);
     const { match: { params } } = props;
+    const idLog = params.userId;
+    const parseToNumber = Number(idLog);
     useEffect(() => {
-
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${params.userId}`)
+        // axios.get(`https://jsonplaceholder.typicode.com/posts/${params.userId}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/`)
             .then(response =>
-                response.data.userId
+                response.data
             )
             .then(data => {
                 setPostsArray(data);
             });
     }, [])
-    const idLog = params.userId;
-    console.log(idLog);
+    const arraCop = postsArray.map(ee => ee.userId === parseToNumber)
+    // .filter((ee2, i) => (
+    //     <div key={i}>
+    //         {ee2.body}
+    //     </div>
+    // ))
     console.log(postsArray);
+    console.log(idLog);
+    console.log(postsArray.map(ee => ee.userId === parseToNumber));
+    console.log(arraCop);
     return (
         <div>
-            {postsArray}
+            <div style={styleComponent}>
+                {arraCop}
+            </div>
         </div>
     )
+}
+const styleComponent = {
+    color: 'black'
 }
