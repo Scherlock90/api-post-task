3d3399b6ae29021e254e11cb75a6ee0b18716090
Sebastian <31919808+Scherlock90@users.noreply.github.com>
2019-05-30 19:10:10 +0200

    adding modal post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import User from './User';
+import PersonList from './PersonList';
 
 export default function Home() {
   return (
       <div className="mainContainer">
         <User />
+        {/* <PersonList /> */}
       </div>
   )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import ReactModal from 'react-modal';
 import axios from 'axios';
-import '../Styles/main.css';
 import TextTruncate from 'react-text-truncate';
+import '../Styles/main.css';
+
 
 export default function Post(props) {
 
     const [postsArray, setPostsArray] = useState([]);
     const [authorName, setAuthorName] = useState([]);
+    const [title, setTitleAdd] = useState('');
+    const [body, setBodyAdd] = useState('');
+    const [modalMainOpen, setModalMainOpen] = useState(false);
 
     const { match: { params } } = props;
     const idLog = params.userId;
     const parseToNumber = Number(idLog);
 
     const URL = 'https://jsonplaceholder.typicode.com';
+
+
+    function toggleModal(modalMainOpen) {
+        setModalMainOpen(true)
+        return modalMainOpen
+    }
+    function closeModal(modalMainOpen) {
+        setModalMainOpen(false)
+        return modalMainOpen
+    }
+
+    const handleChangeTitle = event => {
+        setTitleAdd(event.target.value)
+    }
+    const handleChangeBody = event => {
+        setBodyAdd(event.target.value)
+    }
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        axios.post(`${URL}/posts`, { title: title, body: body })
+            .then(res => {
+                setTitleAdd(res.data.title);
+                setBodyAdd(res.data.body);
+                console.log(res);
+                console.log(res.data);
+            })
+    }
 
     useEffect(() => {
         axios.get(`${URL}/posts/`)
@@ -58,14 +91,13 @@ export default function Post(props) {
                     <div>
                         <div className="uk-card uk-card-body main-cards-posts-right">
                             <h3 className="uk-card-title">
-                                <Link className="arrow-back" to="/#">
-                                    <span uk-icon="icon:  plus-circle; ratio: 2"></span>
-                                </Link>
+                                <span uk-icon="icon:  plus-circle; ratio: 2" onClick={toggleModal}></span>
                             </h3>
                         </div>
                     </div>
                 </div>
             </div>
+            {/* <div> {title}</div> */}
             {arraCop.map((postsUsers, i) => {
                 return (
                     <div className="container-post-cards" key={i} >
@@ -99,6 +131,48 @@ export default function Post(props) {
                 );
             })
             }
+            <ReactModal
+                isOpen={modalMainOpen}
+                contentLabel="onRequestClose Example"
+                onRequestClose={closeModal}
+                className="Modal"
+                overlayClassName="Overlay mainOverlay"
+            >
+                <div className="containerModal">
+                    <div className="containerMyModal" >
+                        <div className="cardPost">
+                            <div className="box">
+                                <div className="little-add-post-title">Add post</div>
+                                <div className="bigger-add-post-title">Add post</div>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="cotainer-label">
+                                        <label>
+                                            <div className="container-input-label">
+                                                <div className="title-span-add-post">Title</div>
+                                                <div>
+                                                    <input type="text" name="title" value={title} placeholder="Title" onChange={handleChangeTitle} />
+                                                </div>
+                                            </div>
+                                        </label>
+                                        <label>
+                                            <div className="container-input-label">
+                                                <div className="body-span-add-post">Body</div>
+                                                <div>
+                                                    <input type="text" name="body" value={body} placeholder="Body" onChange={handleChangeBody} />
+                                                </div>
+                                            </div>
+                                        </label>
+                                    </div>
+                                    <div>
+                                        <button className="uk-button uk-button-primary" type="submit">Save</button>
+                                        <button className="uk-button uk-button-secondary" onClick={closeModal}>Cancel</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </ReactModal>
         </div>
     )
 }
