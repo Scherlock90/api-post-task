fdb7c4f3c31d49a4e97e3669e26a8ba2c56da767
Sebastian <31919808+Scherlock90@users.noreply.github.com>
2019-06-08 12:31:18 +0200

    adding title in component Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import User from './User';
 
+const styleUsers = {
+  fontSize: '220%',
+  textAlign: 'center',
+  margin: '1em'
+}
+
 export default function Home() {
   return (
-      <div className="mainContainer">
+    <div className="mainContainer">
+      <div style={styleUsers}>API <br/> Users-Post-Comments</div>
+      <div>
         <User />
       </div>
+    </div>
   )
 }
