df6e1b19a873408abd797cce0b6937f48aca9198
Sebastian Napora <sebastian.napora@intive.com>
2020-04-16 16:27:04 +0200

    gitignore change comment build

---
+++ build/precache-manifest.d9cfd042760ccc68e6fa489f7b8cae8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76373dad80c5a9d117c3dbd3e27e82a",
+    "url": "/api-post-task/index.html"
+  },
+  {
+    "revision": "3decdcacd692ccf89c79",
+    "url": "/api-post-task/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "9feaf3389d5be6646b41",
+    "url": "/api-post-task/static/css/main.238a442d.chunk.css"
+  },
+  {
+    "revision": "3decdcacd692ccf89c79",
+    "url": "/api-post-task/static/js/2.ee1b1e4d.chunk.js"
+  },
+  {
+    "revision": "9feaf3389d5be6646b41",
+    "url": "/api-post-task/static/js/main.ce261b26.chunk.js"
+  },
+  {
+    "revision": "b337d87fa545d37c3458",
+    "url": "/api-post-task/static/js/runtime~main.0da33bdd.js"
+  }
+]);
