673d4e825bcc7612e176a41412b7ed307065c10a
Sebastian Napora <sebastian.napora@intive.com>
2020-04-16 16:17:40 +0200

    change test in wrapper

--- src/components/common/wrapper/Wrapper.jsx
+++ src/components/common/wrapper/Wrapper.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 export const Wrapper = ({ name, children }) =>
     <tr>
-        <td className="body-container-form">{ name }</td>
+        <td className="body-container-form">
+            { name }
+        </td>
         <td className="body-container-form2">
             { children }
         </td>
