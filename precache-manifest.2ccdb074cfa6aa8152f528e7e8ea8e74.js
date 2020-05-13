e85e5af5ef61b8c1cd3366ad792823ed4826cdb2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-13 09:50:03 +0000

    Deploy Scherlock90/api-post-task to github.com/Scherlock90/api-post-task.git:gh-pages

---
+++ precache-manifest.2ccdb074cfa6aa8152f528e7e8ea8e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a781af9de8ac94a46142b4eeb72ee6e7",
+    "url": "/api-post-task/index.html"
+  },
+  {
+    "revision": "7569e37636fb53996ec1",
+    "url": "/api-post-task/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "e2491110ff06ff845971",
+    "url": "/api-post-task/static/css/main.8427f329.chunk.css"
+  },
+  {
+    "revision": "7569e37636fb53996ec1",
+    "url": "/api-post-task/static/js/2.5744113e.chunk.js"
+  },
+  {
+    "revision": "e2491110ff06ff845971",
+    "url": "/api-post-task/static/js/main.4b1bda99.chunk.js"
+  },
+  {
+    "revision": "b337d87fa545d37c3458",
+    "url": "/api-post-task/static/js/runtime~main.0da33bdd.js"
+  }
+]);
